3063283060a0e1c011d09ba91d83622b25ff3e21
Evan Shimizu <ebshimizu@gmail.com>
2020-06-09 22:05:02 -0400

    signups, match schema changes

---
+++ commands/close.js
@@ -0,0 +1,25 @@
+module.exports = {
+  name: 'close',
+  description: 'Close registration.',
+  guildOnly: true,
+  permissions: ['ADMINISTRATOR'],
+  async execute(message, args, db) {
+    message.delete();
+
+    // check for active tournaments in the server
+    const active = await db.tournament.active(message.guild.id);
+
+    if (active.length == 0) {
+      message.reply('No active tournament in this server.');
+      return;
+    }
+
+    // close registration
+    await active[0].update({
+      $set: {
+        signupsOpen: false,
+      },
+    });
+    message.reply(`Registration for **${active[0].name}** is now closed.`);
+  },
+};
